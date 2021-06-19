caf6bd7c1fffbe864ea4df1b4fdc7a9c7b7ec3b5
cubotv68 <86035392+cubotv68@users.noreply.github.com>
2021-06-19 14:05:10 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,7 @@
+./ethminer --farm-recheck 400 -U \
+                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h@eu1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h@us1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h@eu2.ethermine.org:5555 \
+                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h@us2.ethermine.org:5555 \
+                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.h@asia1.ethermine.org:5555 \
+                    	
